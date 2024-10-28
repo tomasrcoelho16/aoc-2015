45ef2d894ed43d4fc6cfdcd030925a5ca067b23d
tomasrcoelho <tomasrcoelho@gmail.com>
2024-10-28 16:21:43 +0000

    aoc 2016 1 & 2 done

--- 2015-6.py
+++ 2015-6.py
@@ -1,6 +1,8 @@
+import numpy as np
+
 def lit_lights():
     file = open('2015-6-input.txt', 'r')
-    on_lights = []
+    matrix = np.zeros((1000,1000), dtype=int)
     while True:
         content = file.readline()
         if not content: break
@@ -12,19 +14,29 @@ def lit_lights():
         finish_coords = parcels[2].split(',')
         for i in range(int(init_coords[0]), int(finish_coords[0])+1):
             for j in range(int(init_coords[1]), int(finish_coords[1])+1):
-                if (i,j) in on_lights:
-                    if action == 'on':
-                        continue
-                    else:
-                        on_lights.remove((i,j))
-                        continue
-                if action == 'off':
-                    continue
-                else:
-                    on_lights.append((i,j))
-                    continue
-    print(len(on_lights))
+                if action == 'on': matrix[i,j] = 1; continue;
+                if action == 'off': matrix[i,j] = 0; continue;
+                if action == 'toggle' and matrix[i,j] == 0: matrix[i,j] = 1
+                else: matrix[i,j] = 0
+    print(matrix.sum())
 
+def total_brightness():
+    file = open('2015-6-input.txt', 'r')
+    matrix = np.zeros((1000,1000), dtype=int)
+    while True:
+        content = file.readline()
+        if not content: break
+        parcels = content.split(' ')
+        action = 'on'
+        if parcels[0] == 'turn': action = parcels[1]; parcels.pop(1); parcels.pop(0);
+        else: action = parcels[0]; parcels.pop(0)
+        init_coords = parcels[0].split(',')
+        finish_coords = parcels[2].split(',')
+        for i in range(int(init_coords[0]), int(finish_coords[0])+1):
+            for j in range(int(init_coords[1]), int(finish_coords[1])+1):
+                if action == 'on': matrix[i,j] += 1; continue;
+                if action == 'off' and matrix[i,j] > 0: matrix[i,j] -= 1; continue;
+                if action == 'toggle': matrix[i,j] += 2
+    print(matrix.sum())
 
-lit_lights()            
-
+total_brightness()
