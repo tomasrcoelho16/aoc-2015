7216d7644b7f4a4dca10855de0c79b9d243b6352
tomasrcoelho <tomasrcoelho@gmail.com>
2024-11-04 17:19:20 +0000

    progress

--- day-7/main.ts
+++ day-7/main.ts
@@ -1,9 +1,13 @@
-function main() {
-  const path = "input.txt";
-  const file = Bun.file(path);
+async function main() {
+  const file = Bun.file("input.txt");
+  const txt_content = await file.text();
+  const lines = txt_content.split("\n");
   const re = new RegExp(
     /((?<not>NOT) )?((?<numberLeft>[1-9]*)|(?<leftOperand>[a-z]*)) ((?<logic>AND|OR|LSHIFT|RSHIFT) )?(((?<numberRight>[1-9]*)|(?<rightOperand>[a-z]*)) )?-> (?<out>[a-z]*)/,
   );
+  for (var line of lines) {
+    console.log(line);
+  }
 }
 
 main();
