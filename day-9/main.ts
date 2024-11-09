c4ec82ad91dddbdb1741c8872ad39af7727b3913
tomasrcoelho <tomasrcoelho@gmail.com>
2024-11-09 18:32:02 +0000

    day-9 started

--- day-9/main.ts
+++ day-9/main.ts
@@ -1,15 +1,35 @@
+import type { Flight } from "./utils";
+
 async function main() {
   let parts = await readFile();
+
+  let routes: Flight[] = toRoute(parts);
+
+  routes.sort((a, b) => a.dist - b.dist);
+
+  let visited_cities: string[] = [];
+  let distance = 0;
+  for (let flight of routes) {
+    let { fcity, scity, dist } = flight;
+    if(visited_cities.includes(fcity))
+  }
+}
+
+function toRoute(parts: string[]): Flight[] {
+  let routes: Flight[] = [];
+
   const re = new RegExp(
     /(?<fcity>[a-zA-Z]*) to (?<scity>[a-zA-Z]*) = (?<dist>[0-9]*)/,
   );
-  console.log(parts);
+
   for (let part of parts) {
     let expr = re.exec(part);
     if (expr) {
-      console.log(expr.groups);
+      routes.push(expr.groups as unknown as Flight);
     }
   }
+
+  return routes;
 }
 
 async function readFile(): Promise<string[]> {
---
+++ day-9/utils.ts
@@ -0,0 +1,5 @@
+export type Flight = {
+  fcity: string;
+  scity: string;
+  dist: number;
+};
